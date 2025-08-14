34c374db13dc439497ed05b6c81fbb03943dcd72
Shankyindia <s.shekhar2197@gmail.com>
2025-08-14 11:24:48 +0530

    key-vault name changed

--- todo_main/main.tf
+++ todo_main/main.tf
@@ -1,23 +1,9 @@
 module "todo_rg" {
-  source = "../todo_infra/todo-rg"
+  source   = "../todo_infra/todo-rg"
   rg_name  = "todo-rg"
   location = "East US"
 }
-module "todo_rg1" {
-  source = "../todo_infra/todo-rg"
-  rg_name  = "todo-rg1"
-  location = "East US"
-}
-module "todo_rg3" {
-  source = "../todo_infra/todo-rg"
-  rg_name  = "todo-rg3"
-  location = "East US"
-}
-module "todo_rg4" {
-  source = "../todo_infra/todo-rg"
-  rg_name  = "todo-rg4"
-  location = "East US"
-}
+
 module "todo_vnet" {
   depends_on = [module.todo_rg]
   source     = "../todo_infra/todo-vnet"
@@ -26,91 +12,91 @@ module "todo_vnet" {
   location   = "East US"
 }
 module "subnet1" {
-  depends_on  = [module.todo_vnet]
-  source      = "../todo_infra/todo-subnet"
-  subnet_name = "todo-subnet"
-  rg_name     = "todo-rg"
-  vnet_name = "vnet1"
+  depends_on       = [module.todo_vnet]
+  source           = "../todo_infra/todo-subnet"
+  subnet_name      = "todo-subnet"
+  rg_name          = "todo-rg"
+  vnet_name        = "vnet1"
   address_prefixes = ["10.0.1.0/24"]
 }
-module "key_vault_todo"{
-  source = "../todo_infra/key_vault"
+module "key_vault_todo" {
+  source         = "../todo_infra/key_vault"
+  depends_on     = [module.todo_rg]
+  key_vault_name = "tijori0000"
+  rg_name        = "todo-rg"
+  location       = "East US"
+
+}
+
+module "username" {
+  source         = "../todo_infra/key_vault_secret"
+  depends_on     = [module.key_vault_todo]
+  secret_name    = "fe-username"
+  secret_value   = "adminuser"
+  rg_name        = "todo-rg"
+  key_vault_name = "tijori0000"
+}
+
+module "password" {
+  source         = "../todo_infra/key_vault_secret"
+  depends_on     = [module.key_vault_todo]
+  secret_name    = "fe-password"
+  secret_value   = "Shashank@1234"
+  rg_name        = "todo-rg"
+  key_vault_name = "tijori0000"
+}
+module "pip_todo" {
+  source     = "../todo_infra/public_ip"
   depends_on = [module.todo_rg]
-  key_vault_name = "tala-chabhi"
-  rg_name = "todo-rg"
-  location = "East US"
-
-}
-
-module "username"{
-  source = "../todo_infra/key_vault_secret"
-  depends_on = [module.key_vault_todo]
-  secret_name = "fe-username"
-  secret_value = "adminuser"
-  rg_name = "todo-rg"
-  key_vault_name = "tala-chabhi"
-}
-
-module "password"{
-  source = "../todo_infra/key_vault_secret"
-  depends_on = [module.key_vault_todo]
-  secret_name = "fe-password"
-  secret_value = "Shashank@1234"
-  rg_name = "todo-rg"
-  key_vault_name = "tala-chabhi"
-}
-module "pip_todo"{
-  source = "../todo_infra/public_ip"
-  depends_on = [module.todo_rg]
-  pip_name = "todo_access_ip"
-  rg_name = "todo-rg"
-  location = "East US"
+  pip_name   = "todo_access_ip"
+  rg_name    = "todo-rg"
+  location   = "East US"
 }
 
 module "todo_vm" {
-  depends_on = [ module.pip_todo,module.todo_rg,module.todo_vnet,module.key_vault_todo ]
-  source = "../todo_infra/todo-vm"
-  nic_name = "todo-nic_fe"
-  rg_name = "todo-rg"
-  location = "East US"
-  vm_name = "todo-fe-vm"
-  vnet_name = "vnet1"
-  subnet_name = "todo-subnet"
-  pip_name = "todo_access_ip"
+  depends_on           = [module.pip_todo, module.todo_rg, module.todo_vnet, module.key_vault_todo]
+  source               = "../todo_infra/todo-vm"
+  nic_name             = "todo-nic_fe"
+  rg_name              = "todo-rg"
+  location             = "East US"
+  vm_name              = "todo-fe-vm"
+  vnet_name            = "vnet1"
+  subnet_name          = "todo-subnet"
+  pip_name             = "todo_access_ip"
   username_secret_name = "fe-username"
   password_secret_name = "fe-password"
-  key_vault_name = "tala-chabhi"
+  key_vault_name       = "tijori0000"
 }
 
 module "subnet2" {
-  depends_on  = [module.todo_vnet]
-  source      = "../todo_infra/todo-subnet"
-  subnet_name = "todo-subnet-be"
-  rg_name     = "todo-rg"
-  vnet_name   = "vnet1"
+  depends_on       = [module.todo_vnet]
+  source           = "../todo_infra/todo-subnet"
+  subnet_name      = "todo-subnet-be"
+  rg_name          = "todo-rg"
+  vnet_name        = "vnet1"
   address_prefixes = ["10.0.2.0/24"]
 }
 
-module "pip_todo_be"{
-  source = "../todo_infra/public_ip"
+module "pip_todo_be" {
+  source     = "../todo_infra/public_ip"
   depends_on = [module.todo_rg]
-  pip_name = "todo_access_ip_be"
-  rg_name = "todo-rg"
-  location = "East US"
+  pip_name   = "todo_access_ip_be"
+  rg_name    = "todo-rg"
+  location   = "East US"
 }
 
 
 module "todo_vm_be" {
-  depends_on = [ module.todo_rg,module.todo_vnet,module.key_vault_todo,module.pip_todo_be,module.subnet2 ]
-  source = "../todo_infra/todo-vm"
-  nic_name = "todo-nic_be"
-  rg_name = "todo-rg"
-  location = "East US"
-  vm_name = "todo-be-vm"
-  vnet_name = "vnet1"
-  subnet_name = "todo-subnet-be"
-  pip_name = "todo_access_ip_be"
+  depends_on           = [module.todo_rg, module.todo_vnet, module.key_vault_todo, module.pip_todo_be, module.subnet2]
+  source               = "../todo_infra/todo-vm"
+  nic_name             = "todo-nic_be"
+  rg_name              = "todo-rg"
+  location             = "East US"
+  vm_name              = "todo-be-vm"
+  vnet_name            = "vnet1"
+  subnet_name          = "todo-subnet-be"
+  pip_name             = "todo_access_ip_be"
   username_secret_name = "fe-username"
   password_secret_name = "fe-password"
-  key_vault_name = "tala-chabhi"
+  key_vault_name       = "tijori0000"
 }
--- todo_main/provider.tf
+++ todo_main/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.28.0"
+      version = "4.39.0"
     }
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "3f7c4872-f198-45b7-984d-0ab188b016e6"
+  subscription_id = "3be29ba2-b31c-4fbd-a7bb-c51bf50ef740"
 }
